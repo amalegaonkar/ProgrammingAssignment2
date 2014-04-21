4750c133514481a0b9abcdec00c2fb608ce276ac
Ashutosh Malegaonkar <amalegaonkar@gmail.com>
2014-04-20 18:49:46 -0700

    Added actual code to the stubs given by the instructor

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Creates a set of functions (getters and setters) for matrix and the inverse:
+# set   : set the value of the matrix
+# get   : get the value of the matrix
+# setinverse: set the value of the inverse matrix
+# getinverse: get the value of the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
+        inverse   <- NULL               # inverse
+        set <- function(y)              # construct a func that sets x to its input param
+        {
+                x <<- y                 # set to new matrix
+                inverse <<- NULL        # matrix had changed - inverse sets to 0
+        }
+        
+        get     <- function()    x                # construct a func that returns x
+        setinverse <- function(inv) inverse <<- inv        # construct a func that sets "inverse" to its input param
+        getinverse <- function()    inverse                # construct a func that returns i
+        
+        list(set = set, get = get, setinv = setinverse, getinv = getinverse) # return list of functions
 
 }
 
@@ -11,5 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        inv <- x$getinv()                       # try getting inverse matrix from the cache
+        if (!is.null(inv))                          # if inverse matrix was calculated before
+        {
+                message("This is from the Cache")
+                return(inv)                             # return the cached inverse
+        }
+        
+        data <- x$get()                           # get the cached matrix
+        inv    <- solve(data, ...)                # caclulate its inverse
+        
+        x$setinv(inv)                               # cache the inverse matrix
+        inv                                         # return the inverse matrix
         ## Return a matrix that is the inverse of 'x'
 }
